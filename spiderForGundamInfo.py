adc6b5bd0f4a1aa2456123ec641f727dca4b7880
Matthew Lewis <matthew.knight.lewis@gmail.com>
2020-10-29 23:49:34 -0400

    thats all of them

--- spiderForGundamInfo.py
+++ spiderForGundamInfo.py
@@ -17,12 +17,10 @@ class Gundam:
         pass
 
 
-f = open('data.txt', 'a')
-
 arrayOfObjects = []
 
-i = 0
-while i < 400:
+i = 1200
+while i < (len(urlData)-10):
     i += 1
     print(urlData[i])
     tempPage = urllib.request.urlopen(urlData[i])
@@ -34,7 +32,7 @@ while i < 400:
     lengthOfLabels = (len(tagLabels))
     if lengthOfLabels == lengthOfValues:
         tempGundam = Gundam() 
-        if lengthOfLabels == lengthOfValues:
+        if lengthOfLabels == lengthOfValues and lengthOfLabels != 0:
             j = 0
             while j < lengthOfLabels:
                 setattr(tempGundam, tagLabels[j].text, tagValues[j].text)
@@ -42,38 +40,13 @@ while i < 400:
             arrayOfObjects.append(json.dumps(tempGundam.__dict__, indent=4))
 
 
-f = open('data.txt', 'w')
+f = open('twelveToEnd.txt', 'w')
 f.write('[')
 
 for item in arrayOfObjects:
     f.write(item)
+    f.write(',')
 
 f.write(']')
 f.close()
 print('end')
-
-
-# #Sample page to test data extraction
-
-# f = open('data.txt', 'w')
-# j = 200
-# while j < 205:
-
-#     print(urlList2[j])
-
-#     newPage = urllib.request.urlopen(urlList2[j])
-
-#     newSoup = BeautifulSoup(newPage, features='lxml')
-
-#     infoIwant = newSoup.find_all("div", class_="pi-data-value", recursive=True)
-    
-#     for item in infoIWant:
-#         f.write(str(item) + '\r\n\r\n\r\n')
-
-#     print(j)
-#     j += 1
-#     time.sleep(5)
-
-
-# f.close()
-# print('write complete')
